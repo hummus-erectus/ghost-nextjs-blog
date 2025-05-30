6a6888015bbbb060fef09559e6daa400b5f8b2ef
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-05-30 13:58:23 +0900

    Include domain for avatar images

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["blog.cageundefined.org"],
+    domains: ["blog.cageundefined.org", "www.gravatar.com"],
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "blog.cageundefined.org",
         pathname: "/content/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+        pathname: "/**",
       },
     ],
   },
