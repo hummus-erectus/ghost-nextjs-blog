1251f92b30bdbafd974cadd82d3ecc0a11d5ddd5
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-05-30 12:54:14 +0900

    Install Ghost Content API helper and create getPosts function

---
+++ src/lib/post.ts
@@ -0,0 +1,28 @@
+import GhostContentAPI from "@tryghost/content-api";
+import { PostsOrPages } from "@tryghost/content-api";
+
+// Validate environment variables
+if (!process.env.GHOST_URL) {
+  throw new Error('GHOST_URL environment variable is not defined');
+}
+
+if (!process.env.GHOST_CONTENT_API_KEY) {
+  throw new Error('GHOST_CONTENT_API_KEY environment variable is not defined');
+}
+
+const api = new GhostContentAPI({
+  url: process.env.GHOST_URL as string,
+  key: process.env.GHOST_CONTENT_API_KEY as string,
+  version: "v5.0"
+});
+
+export async function getPosts(): Promise<PostsOrPages | undefined> {
+  try {
+    return await api.posts.browse({
+      limit: "all"
+    });
+  } catch (err: unknown) {
+    console.error(err);
+    return undefined;
+  }
+}
